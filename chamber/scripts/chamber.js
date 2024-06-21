62669e2e1973b232287c4b765aa360fde3bc571b
eworsham <eworsham64@gmail.com>
2024-06-21 18:23:22 -0400

    Header and nav

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,9 @@
+/**************************************************** 
+*
+*           Common for all pages
+*
+****************************************************/
+
 // Populate Current Year and Last Modified Date
 const currentYearSelector = document.querySelector('#currentYear');
 const lastModifiedSelector = document.querySelector('#lastModified');
@@ -6,3 +12,12 @@ const today = new Date();
 
 currentYearSelector.textContent = today.getFullYear();
 lastModifiedSelector.textContent = `Last Modified: ${document.lastModified}`;
+
+// Navigation for mobile view
+const menuButtonSelector = document.querySelector('#menu');
+const navigationSelector = document.querySelector('#navigation');
+
+menuButtonSelector.addEventListener('click', () => {
+    menuButtonSelector.classList.toggle('open');
+    navigationSelector.classList.toggle('open');
+});
