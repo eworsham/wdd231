7f45caff81e78e148022568fced7931925a5413c
eworsham <eworsham64@gmail.com>
2024-07-20 21:03:55 -0400

    Good progress on the news and events page

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -22,47 +22,90 @@ menuButtonSelector.addEventListener('click', () => {
     animatedMenuSelector.classList.toggle('open');
 });
 
+/**************************************************** 
+*
+*           News and Events page
+*
+****************************************************/
+const newsAndEventsSelector = document.querySelector('.news-and-events');
 
-
-
-
-
-// Messing around with ticketmaster api
-const atlantaEventsSelector = document.querySelector('#atlantaEvents');
-const url = 'https://app.ticketmaster.com/discovery/v2/events.json?size=3&city=Atlanta&apikey=SYT8UN9ZN1ayqnA94BwPsxEFcwA0veU1'
-
-async function getEvents(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const events = data._embedded.events;
-    console.log(events);
+function createLocalEventCard(events) {
+    const localEventsSelector = document.querySelector('#localEvents');
 
     events.forEach(event => {
         const name = event.name;
-        const venue = event._embedded.venues[0].name
-        const date = event.dates.start.localDate;
-        const priceLow = event.priceRanges[0].min;
-        const priceHigh = event.priceRanges[0].max;
-
-        console.log(`
-            Event Name: ${name},
-            Event Date: ${date},
-            Event Location: ${venue},
-            Event Fee: $${priceLow}-$${priceHigh}
-        `);
+        const eventInfo = {
+            "venue": event.location,
+            "date": event.date,
+            "priceRange": `$${event.fee}`
+        }
 
         const newEvent = document.createElement('div');
-        newEvent.setAttribute('class', 'event shadow rounded-border');
+        newEvent.setAttribute('class', 'event-card rounded-border');
         newEvent.innerHTML = `
-            <p>Event Name: ${name}</p>
-            <p>Event Date: ${date}</p>
-            <p>Event Location: ${venue}</p>
-            <p>Event Fee: $${priceLow}-$${priceHigh}</p>
+            <h3>${name}</h3>
+            <p>Click for more details</p>
         `;
 
-        atlantaEventsSelector.appendChild(newEvent);
+        newEvent.addEventListener('click', () => {
+            displayEventInfo(eventInfo);
+        });
+
+        localEventsSelector.appendChild(newEvent);
     });
 }
 
-getEvents(url);
+function createAtlantaEventCard(events) {
+    const atlantaEventsSelector = document.querySelector('#atlantaEvents');
+
+    events.forEach(event => {
+        const name = event.name;
+        const eventInfo = {
+            "venue": event._embedded.venues[0].name,
+            "date": event.dates.start.localDate,
+            "priceRange": `$${event.priceRanges[0].min}-$${event.priceRanges[0].max}`
+        }
+
+        const newEvent = document.createElement('div');
+        newEvent.setAttribute('class', 'event-card rounded-border');
+        newEvent.innerHTML = `
+            <h3>${name}</h3>
+            <p>Click for more details</p>
+        `;
+
+        newEvent.addEventListener('click', () => {
+            displayEventInfo(eventInfo);
+        });
+
+        atlantaEventsSelector.appendChild(newEvent); 
+    });
+}
+
+function displayEventInfo(eventInfo) {
+    console.log(eventInfo); //FIXME
+}
+
+async function fetchLocalEventData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    const events = data.localEvents;
+    createLocalEventCard(events);
+}
+
+async function fetchAtlantaEventData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    const events = data._embedded.events;
+    createAtlantaEventCard(events);
+}
+
+if (newsAndEventsSelector) {
+    // Create and display local event cards
+    const localUrl = 'https://eworsham.github.io/wdd231/project/data/events.json'; //FIXME
+    fetchLocalEventData(localUrl);
+    
+    
+    // Create and display Atlanta event cards
+    const atlantaUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?size=3&city=Atlanta&apikey=SYT8UN9ZN1ayqnA94BwPsxEFcwA0veU1'
+    fetchAtlantaEventData(atlantaUrl);
+}
